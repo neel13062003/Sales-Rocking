2927bec21be546d37ad7d8382733006b0718e6ce
neelnsoni13 <neelnsoni13@gmail.com>
2024-10-04 10:11:32 +0530

    correct it json file

--- main.py
+++ main.py
@@ -15,7 +15,25 @@ def convert_google_drive_link_to_direct_url(link):
 
 # Google Sheets connection setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name(r"./oddoo.json", scope)
+
+
+creds_dict = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"].replace('\\n', '\n'),  # Handle newline characters
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+}
+
+# creds = ServiceAccountCredentials.from_json_keyfile_name(r"./oddoo.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+
 client = gspread.authorize(creds)
 
 # Get data from Google Sheets
