9bbaf2c9c26c5d2edebe4abef79d34b2efeb40e4
neelnsoni13 <neelnsoni13@gmail.com>
2024-12-21 14:12:02 +0530

    live

--- main.py
+++ main.py
@@ -22,21 +22,21 @@ def convert_google_drive_link_to_direct_url(link):
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
 
-# creds_dict = {
-#     "type": st.secrets["gcp_service_account"]["type"],
-#     "project_id": st.secrets["gcp_service_account"]["project_id"],
-#     "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-#     "private_key": st.secrets["gcp_service_account"]["private_key"].replace('\\n', '\n'),  # Handle newline characters
-#     "client_email": st.secrets["gcp_service_account"]["client_email"],
-#     "client_id": st.secrets["gcp_service_account"]["client_id"],
-#     "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-#     "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-#     "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-#     "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-# }
+creds_dict = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"].replace('\\n', '\n'),  # Handle newline characters
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+}
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(r"../ENV/key.json", scope)
-# creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+# creds = ServiceAccountCredentials.from_json_keyfile_name(r"../ENV/key.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 
 
 client = gspread.authorize(creds)
